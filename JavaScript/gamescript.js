6ae48e7d760cb4b9569a35ad9f4194e1a78b606e
shaneswezey <shanedswezey@gmail.com>
2018-05-02 14:45:46 -0700

    added initial modal window for game set up

--- JavaScript/gamescript.js
+++ JavaScript/gamescript.js
@@ -6,3 +6,36 @@ const toeBoard = [
 
 var isWinner = false;
 
+var game = {
+    player1: '',
+    player2: '',
+    winner: '',
+    moves: 1
+}
+
+let modal = document.getElementById('gameSetUp');
+let startButton = document.getElementById('newGame');
+let resetButton = document.getElementById('resetGame');
+let modalClose = document.getElementsByClassName('close')[0];
+
+startButton.onclick = function() {
+    modal.style.display = "block";
+};
+
+resetButton.onclick = function() {
+    alert("This is working!");
+};
+
+modalClose.onclick = function() {
+    modal.style.display = "none";
+};
+
+function setGameUp() {
+
+}
+
+
+
+// minimax algorithm to determine all possible moves in current state of tic tac toe game.
+function minimax(game) {
+}
